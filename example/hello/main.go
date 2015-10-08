7204b97c936e860f8704ea1bfaa3a3e935e6cf60
C@Z <cz.theng@gmail.com>
2015-10-08 20:05:51 +0800

    Init

---
+++ example/hello/main.go
@@ -0,0 +1,13 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package main
+
+import (
+	"github.com/cz-it/ridge"
+)
+
+func main() {
+	ridge.Run()
+}
---
+++ ridge.go
@@ -0,0 +1,13 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package ridge
+
+import (
+	"fmt"
+)
+
+func Run() {
+	fmt.Println("Ridge.Start...")
+}
